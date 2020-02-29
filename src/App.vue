2f1261a007db487a3e9cb205957dc84a76a4a261
mholwill <m_holwill@hotmail.co.uk>
2020-02-29 10:10:17 +0000

    Map added to issTracker Component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <div id="app">
     <h1>The International Space Station</h1>
     <h2>{{issPosition.latitude}}</h2>
-    <h1>{{this.astros[0].name}}</h1>
+    <!-- <h1>{{this.astros[0].name}}</h1> -->
     <issTracker :issPosition="issPosition"></issTracker>
   </div>
 </template>
 
 <script>
+import { eventBus } from "@/main.js";
 import IssMapTracker from './components/IssMapTracker.vue'
 
 export default {
@@ -34,9 +35,9 @@ export default {
     .then(res => res.json())
     .then(astrosData => this.astros = astrosData.people)
 
-    fetch('http://api.open-notify.org/iss-pass.json?lat=55.947145&lon=-3.202079&alt=20&n=5')
-    .then(res => res.json())
-    .then(issPassData => this.issPass = issPassData)
+    // fetch('http://api.open-notify.org/iss-pass.json?lat=55.947145&lon=-3.202079&alt=20&n=5')
+    // .then(res => res.json())
+    // .then(issPassData => this.issPass = issPassData)
   }
 }
 </script>
--- src/components/IssMapTracker.vue
+++ src/components/IssMapTracker.vue
@@ -1,15 +1,50 @@
 <template lang="html">
   <div class="">
     <h1>Hello</h1>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
+import L from 'leaflet';
+
 export default {
   name: "Iss-Tracker",
-  props: ['issPosition']
+  data(){
+    return {
+      map: null,
+      tileLayer: null,
+      layers: []
+    }
+  },
+  props: ['issPosition'],
+  mounted() {
+    this.initMap();
+    this.initLayers();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('map').setView([38.63, -90.23], 12);
+
+      this.tileLayer = L.tileLayer(
+        'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+      {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+      }
+      );
+      this.tileLayer.addTo(this.map);
+    },
+  initLayers() {},
+},
 }
+
 </script>
 
 <style lang="css" scoped>
+
+  #map {
+    height: 200px
+  }
+
 </style>
